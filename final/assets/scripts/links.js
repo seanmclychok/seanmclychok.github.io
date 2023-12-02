662f737bf068a0bb33e527c76298244a6fdce984
Sean McLychok <Sean@MacBook-Pro-6.local>
2023-12-01 17:43:25 -0800

    finished final

---
+++ final/assets/scripts/links.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var wavepic3 = document.querySelector(".wavepic3");
+    wavepic3.addEventListener("click", function () {
+        window.location.href = "ocean.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () { 
+    var wavepic = document.querySelector(".wavepic");
+    wavepic.addEventListener("click", function () {
+        window.location.href = "sands.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () { 
+    var wavepic2 = document.querySelector(".wavepic2");
+    wavepic2.addEventListener("click", function () {
+        window.location.href = "trestles.html";
+    });
+});
